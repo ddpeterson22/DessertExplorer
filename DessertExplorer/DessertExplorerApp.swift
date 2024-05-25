692fac9067aa112e0624e044d36fad169d495fee
taozty <ddpeterson22@gmail.com>
2024-05-25 13:17:38 -0500

    Initial Commit

---
+++ DessertExplorer/DessertExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertExplorerApp.swift
+//  DessertExplorer
+//
+//  Created by Daniel Peterson on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
