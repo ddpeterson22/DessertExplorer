a125a6d4e1e174db02879a8e8e93de5402ce4c76
taozty <ddpeterson22@gmail.com>
2024-05-25 15:16:23 -0500

    Added home screen view stub and view model

--- DessertExplorer/DessertExplorerApp.swift
+++ DessertExplorer/DessertExplorerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DessertExplorerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DessertListView()
         }
     }
 }
---
+++ DessertList/DessertListViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  DessertListViewModel.swift
+//  DessertExplorer
+//
+//  Created by Daniel Peterson on 5/25/24.
+//
+
+import Foundation
+
+class DessertListViewModel {
+  
+  var meals = [Meal]()
+  var selectedMeal: Meal?
+  private let dessertCategory = "dessert"
+  
+  func fetchDesserts(completion: @escaping () -> () ) {
+    
+    MealServices.shared.fetchMeals(for: dessertCategory) { result in
+      switch result {
+        case .failure(let error):
+          print("Error fetching meals. Error: \(error)")
+        case .success(let response):
+          self.meals = response.meals.sorted { $0.name.lowercased() < $1.name.lowercased() }
+          completion()
+          
+          
+      }
+    }
+  }
+}
